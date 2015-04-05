14922ffb2f855ed211ce89daaf40c3e895184e6b
Michael Becco <e4e6d4d5@gmail.com>
2015-04-05 11:57:07 -0700

    Migrate avatar_processed to user model
    Add code to process image as a background job with sidekiq

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
+
+  after_save :enqueue_avatar
+
+  def enqueue_avatar
+    AvatarWorker.perform_async(id, key) if key.present?
+  end
+
+  class AvatarWorker
+    include Sidekiq::Worker
+
+    def perform(id, key)
+      avatar = User.find(id)
+      user.key = key
+      user.remote_avatar_url = user.avatar.direct_fog_url(with_path: true)
+      user.save!
+      user.update_column(:avatar_processed, true)
+    end
+  end
 end
